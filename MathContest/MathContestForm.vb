4071172e4257c2e7a2b51b9e46a725b44dcc119b
AlexWheelock <130195109+AlexWheelock@users.noreply.github.com>
2024-03-29 00:27:58 -0600

    tooltip and annotation still needed

--- MathContest/MathContestForm.vb
+++ MathContest/MathContestForm.vb
@@ -6,10 +6,9 @@
 
 Option Explicit On
 Option Strict On
+
 Imports System.CodeDom.Compiler
 Imports System.Security.Cryptography.X509Certificates
-
-
 
 'TODO
 '[x] Create a sub to clear/set default settings
@@ -21,8 +20,8 @@ Imports System.Security.Cryptography.X509Certificates
 '[x] Create a way to handle the math problem type for the problems
 '[x] Get the correct answer for the math problem
 '[x] Check student answers, compare it to the correct answer
-'[ ] Run through 5 problems and give the score to the student
-'[ ] Allow the student to check the amount of problems they have gotten right during the contest
+'[x] Run through 5 problems and give the score to the student
+'[x] Allow the student to check the amount of problems they have gotten right during the contest
 Public Class MathContestForm
 
     'Clears everything and sets it back to default settings
@@ -40,6 +39,10 @@ Public Class MathContestForm
             DivideRadioButton.Checked = False
             SubmitButton.Enabled = False
             SummaryButton.Enabled = False
+            AddRadioButton.Enabled = False
+            SubtractRadioButton.Enabled = False
+            MultiplyRadioButton.Enabled = False
+            DivideRadioButton.Enabled = False
         Else
             StudentAnswerTextBox.Text = ""
         End If
@@ -47,7 +50,7 @@ Public Class MathContestForm
     End Sub
 
     'Validates the input student information when the submit button is pressed
-    Sub ValidateStudentInformation()
+    Sub ValidateStudentInformation(newContest As Boolean)
         Dim validFields As Boolean = True
         Dim nameTest As Integer = 0
         Dim ageTest As Integer = 0
@@ -55,33 +58,58 @@ Public Class MathContestForm
 
         'Tests to see if the name can be converted to a number, if so it flags it as an error
         'and adds it to the errorMessage
-        Try
-            nameTest = CInt(NameTextBox.Text)
-            NameTextBox.Text = ""
-            NameTextBox.Focus()
-            validFields = False
-            NameTextBox.BackColor = Color.LightYellow
-        Catch ex As Exception
-            'Makes sure that something was put into NameTextBox, if not then it will flag it as an error
-            'and add it to the errorMessage
-            If NameTextBox.Text = "" Then
+        If newContest = False Then
+
+            Try
+                nameTest = CInt(NameTextBox.Text)
                 NameTextBox.Text = ""
                 NameTextBox.Focus()
                 validFields = False
                 NameTextBox.BackColor = Color.LightYellow
-            Else
-                NameTextBox.BackColor = Color.White
-            End If
-        End Try
+            Catch ex As Exception
+                'Makes sure that something was put into NameTextBox, if not then it will flag it as an error
+                'and add it to the errorMessage
+                If NameTextBox.Text = "" Then
+                    NameTextBox.Text = ""
+                    NameTextBox.Focus()
+                    validFields = False
+                    NameTextBox.BackColor = Color.LightYellow
+                Else
+                    NameTextBox.BackColor = Color.White
+                End If
+            End Try
 
-        'Validates the age, verifying that it is within 7-11 and a number
-        Try
-            'Checks to see that the entered age is a number
-            ageTest = CInt(AgeTextBox.Text)
+            'Validates the age, verifying that it is within 7-11 and a number
+            Try
+                'Checks to see that the entered age is a number
+                ageTest = CInt(AgeTextBox.Text)
 
-            'Checks to see that the entered age number is between 7 and 11
-            'If not, it will flag the age as an error, and add it to the errorMessage
-            If ageTest < 7 Then
+                'Checks to see that the entered age number is between 7 and 11
+                'If not, it will flag the age as an error, and add it to the errorMessage
+                If ageTest < 7 Then
+                    If validFields Then
+                        AgeTextBox.Focus()
+                    Else
+                    End If
+                    AgeTextBox.Text = ""
+                    validFields = False
+                    AgeTextBox.BackColor = Color.LightYellow
+                ElseIf ageTest > 11 Then
+                    If validFields Then
+                        AgeTextBox.Focus()
+                    Else
+                    End If
+                    AgeTextBox.Text = ""
+                    validFields = False
+                    AgeTextBox.BackColor = Color.LightYellow
+                Else
+                    AgeTextBox.BackColor = Color.White
+                End If
+            Catch ex As Exception
+                'If the entered age cannot be converted to an integer, then it will flag it as an error
+                'Adds it to the errorMessage
+                'If validFields is already false, then it keeps the focus on the earlier invalid input
+                'else it will set the focus to the AgeTextBox upon failure.
                 If validFields Then
                     AgeTextBox.Focus()
                 Else
@@ -89,48 +117,39 @@ Public Class MathContestForm
                 AgeTextBox.Text = ""
                 validFields = False
                 AgeTextBox.BackColor = Color.LightYellow
-            ElseIf ageTest > 11 Then
-                If validFields Then
-                    AgeTextBox.Focus()
-                Else
-                End If
-                AgeTextBox.Text = ""
-                validFields = False
-                AgeTextBox.BackColor = Color.LightYellow
-            Else
-                AgeTextBox.BackColor = Color.White
-            End If
-        Catch ex As Exception
-            'If the entered age cannot be converted to an integer, then it will flag it as an error
-            'Adds it to the errorMessage
-            'If validFields is already false, then it keeps the focus on the earlier invalid input
-            'else it will set the focus to the AgeTextBox upon failure.
-            If validFields Then
-                AgeTextBox.Focus()
-            Else
-            End If
-            AgeTextBox.Text = ""
-            validFields = False
-            AgeTextBox.BackColor = Color.LightYellow
-        End Try
+            End Try
 
-        Try
-            'Checks to see that the entered grade is a number
-            gradeTest = CInt(GradeTextBox.Text)
+            Try
+                'Checks to see that the entered grade is a number
+                gradeTest = CInt(GradeTextBox.Text)
 
-            'Checks to see that the entered grade number is between 1 and 4
-            'If not, it will flag the grade as an error, and add it to the errorMessage
-            'If validFields is already false then it will keep the focus on the first earlier failed
-            'input, else it will set the focus to GradeTextBox upon failure.
-            If gradeTest < 1 Then
-                If validFields Then
-                    GradeTextBox.Focus()
+                'Checks to see that the entered grade number is between 1 and 4
+                'If not, it will flag the grade as an error, and add it to the errorMessage
+                'If validFields is already false then it will keep the focus on the first earlier failed
+                'input, else it will set the focus to GradeTextBox upon failure.
+                If gradeTest < 1 Then
+                    If validFields Then
+                        GradeTextBox.Focus()
+                    Else
+                    End If
+                    GradeTextBox.Text = ""
+                    validFields = False
+                    GradeTextBox.BackColor = Color.LightYellow
+                ElseIf gradeTest > 4 Then
+                    If validFields Then
+                        GradeTextBox.Focus()
+                    Else
+                    End If
+                    GradeTextBox.Text = ""
+                    validFields = False
+                    GradeTextBox.BackColor = Color.LightYellow
                 Else
+                    GradeTextBox.BackColor = Color.White
                 End If
-                GradeTextBox.Text = ""
-                validFields = False
-                GradeTextBox.BackColor = Color.LightYellow
-            ElseIf gradeTest > 4 Then
+
+            Catch ex As Exception
+                'If the entered grade cannot be converted to an integer, then it will flag it as an error
+                'Adds it to the errorMessage
                 If validFields Then
                     GradeTextBox.Focus()
                 Else
@@ -138,54 +157,65 @@ Public Class MathContestForm
                 GradeTextBox.Text = ""
                 validFields = False
                 GradeTextBox.BackColor = Color.LightYellow
-            Else
-                GradeTextBox.BackColor = Color.White
-            End If
+            End Try
 
-        Catch ex As Exception
-            'If the entered grade cannot be converted to an integer, then it will flag it as an error
-            'Adds it to the errorMessage
-            If validFields Then
-                GradeTextBox.Focus()
+            If validFields = True Then
+                NameTextBox.Enabled = False
+                AgeTextBox.Enabled = False
+                GradeTextBox.Enabled = False
+                AddRadioButton.Enabled = False
+                SubtractRadioButton.Enabled = False
+                MultiplyRadioButton.Enabled = False
+                DivideRadioButton.Enabled = False
+                SubmitButton.Enabled = True
+                SummaryButton.Enabled = True
+                AddRadioButton.Enabled = True
+                SubtractRadioButton.Enabled = True
+                MultiplyRadioButton.Enabled = True
+                DivideRadioButton.Enabled = True
+                NewProblem()
             Else
             End If
-            GradeTextBox.Text = ""
-            validFields = False
-            GradeTextBox.BackColor = Color.LightYellow
-        End Try
-
-        If validFields = True Then
-            NameTextBox.Enabled = False
-            AgeTextBox.Enabled = False
-            GradeTextBox.Enabled = False
-            AddRadioButton.Enabled = False
-            SubtractRadioButton.Enabled = False
-            MultiplyRadioButton.Enabled = False
-            DivideRadioButton.Enabled = False
-            SubmitButton.Enabled = True
-            SummaryButton.Enabled = True
-            StartContest()
         Else
         End If
-
     End Sub
 
-    Sub StartContest()
+    Sub RunContest()
         Dim answerTest As Integer
-        Dim problemNumber As Integer
-        Dim trackScore As Integer
-
-        FirstNumberTextBox.Text = GenerateNumber()
-        SecondNumberTextBox.Text = GenerateNumber()
+        Static problemNumber As Integer
+        Static trackScore As Integer
 
         Try
             answerTest = CInt(StudentAnswerTextBox.Text)
             StudentAnswerTextBox.BackColor = Color.White
-            problemNumber += 1
             If CheckAnswer() Then
                 trackScore += 1
             Else
             End If
+            problemNumber += 1
+            If problemNumber = 5 Then
+                MsgBox($"{NameTextBox.Text} scored {trackScore} out of {problemNumber} problems.")
+                problemNumber = 0
+                trackScore = 0
+                ValidateStudentInformation(True)
+                NameTextBox.Enabled = True
+                AgeTextBox.Enabled = True
+                GradeTextBox.Enabled = True
+                SubmitButton.Enabled = False
+                SummaryButton.Enabled = False
+                StudentAnswerTextBox.Text = ""
+                FirstNumberTextBox.Text = ""
+                SecondNumberTextBox.Text = ""
+                AddRadioButton.Enabled = False
+                SubtractRadioButton.Enabled = False
+                MultiplyRadioButton.Enabled = False
+                DivideRadioButton.Enabled = False
+            Else
+                StudentAnswerTextBox.Text = ""
+                NewProblem()
+            End If
+            StoreRunningCorrect(trackScore)
+            StoreRunningProblem(problemNumber)
         Catch ex As Exception
             If StudentAnswerTextBox.Text = "" Then
                 StudentAnswerTextBox.Text = ""
@@ -199,6 +229,21 @@ Public Class MathContestForm
 
         End Try
     End Sub
+
+    Sub NewProblem()
+        FirstNumberTextBox.Text = GenerateNumber()
+        SecondNumberTextBox.Text = GenerateNumber()
+    End Sub
+
+    Function GenerateNumber() As String
+        Dim number As Integer
+
+        Randomize()
+        number = CInt(Rnd() * 100)
+
+        Return CStr(number)
+    End Function
+
 
     Function CheckAnswer() As Boolean
         Dim correctAnswer As Boolean
@@ -231,22 +276,40 @@ Public Class MathContestForm
         Return correctAnswer
     End Function
 
+
     Sub Summary()
-        MsgBox($"{NameTextBox.Text} has a score of ")
+        Dim currentCorrect As Integer
+        Dim currentProblem As Integer
+
+        currentCorrect = StoreRunningCorrect(99)
+        currentProblem = StoreRunningProblem(99)
+
+        MsgBox($"{NameTextBox.Text} has a current score of {currentCorrect} out of {currentProblem} possible.")
     End Sub
 
-    Function GenerateNumber() As String
-        Dim number As Integer
 
-        Randomize()
-        number = CInt(Rnd() * 100)
+    Function StoreRunningProblem(problemNumber As Integer) As Integer
+        Static currentProblem As Integer
 
-        Return CStr(number)
+        If problemNumber = 99 Then
+        Else
+            currentProblem = problemNumber
+        End If
+
+        Return currentProblem
     End Function
 
-    Sub SolveProblem()
+    Function StoreRunningCorrect(trackScore As Integer) As Integer
+        Static currentCorrect As Integer
 
-    End Sub
+        If trackScore = 99 Then
+        Else
+            currentCorrect = trackScore
+        End If
+
+        Return currentCorrect
+    End Function
+
 
     'Event handlers below this point
 
@@ -315,19 +378,19 @@ Public Class MathContestForm
     End Sub
 
     Private Sub AddRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles AddRadioButton.CheckedChanged
-
+        NewProblem()
     End Sub
 
     Private Sub SubtractRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles SubtractRadioButton.CheckedChanged
-
+        NewProblem()
     End Sub
 
     Private Sub MultiplyRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles MultiplyRadioButton.CheckedChanged
-
+        NewProblem()
     End Sub
 
     Private Sub DivideRadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles DivideRadioButton.CheckedChanged
-
+        NewProblem()
     End Sub
 
     Private Sub ButtonGroupBox_Enter(sender As Object, e As EventArgs) Handles ButtonGroupBox.Enter
@@ -335,7 +398,7 @@ Public Class MathContestForm
     End Sub
 
     Private Sub SubmitButton_Click(sender As Object, e As EventArgs) Handles SubmitButton.Click
-        StartContest()
+        RunContest()
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
@@ -355,6 +418,6 @@ Public Class MathContestForm
     End Sub
 
     Private Sub NameTextBox_Leave(sender As Object, e As EventArgs) Handles NameTextBox.Leave, AgeTextBox.Leave, GradeTextBox.Leave
-        ValidateStudentInformation()
+        ValidateStudentInformation(False)
     End Sub
 End Class
